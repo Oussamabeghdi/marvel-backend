ae189908fbff0fb75c2076750d3a0c24bdc45df2
oussamab <oussama@MacBook-Pro-de-oussamab.local>
2023-02-19 21:43:45 +0100

    add route

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 require("dotenv").config();
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-// mongoose.connect("mongodb://localhost:27017");
+const userRoutes = require("./routes/user");
+app.use(userRoutes);
+mongoose.set("strictQuery", true);
+mongoose.connect("mongodb://localhost:27017");
 
 app.get("/characters", async (req, res) => {
   try {
@@ -25,6 +27,16 @@ app.get("/characters", async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
+app.get("/character/:characterId", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/character/${req.params.characterId}?apiKey=${process.env.API_KEY}`
+    );
+    res.json();
+  } catch (error) {
+    console.log({ message: error.message });
+  }
+});
 
 app.get("/comics", async (req, res) => {
   try {
@@ -38,6 +50,18 @@ app.get("/comics", async (req, res) => {
     res.json(response.data);
   } catch (error) {
     res.status(400).json({ error: error.message });
+  }
+});
+app.get("/comics/:characterId", async (req, res) => {
+  try {
+    console.log(req.params);
+
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics/${req.params.characterId}&apikey=${process.env.API_KEY}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
 });
 
---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.model(
+  "User",
+
+  {
+    email: String,
+
+    account: {
+      username: String,
+    },
+
+    token: String,
+
+    hash: String,
+    salt: String,
+  }
+);
+module.exports = User;
