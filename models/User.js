0ce22eb12f0978c3b264ed832fd3098e13f7696f
oussamab <oussama@MacBook-Pro-de-oussamab.local>
2023-04-25 14:00:57 +0200

    add favoriteModel

--- models/User.js
+++ models/User.js
@@ -1,17 +1,12 @@
 const mongoose = require("mongoose");
 
-const User = mongoose.model(
-  "User",
-
-  {
-    email: String,
-
-    account: { username: String },
-
-    token: String,
-
-    hash: String,
-    salt: String,
-  }
-);
+const User = mongoose.model("User", {
+  email: String,
+  favoritesCharacters: [String],
+  favoritesComics: [String],
+  account: { username: String },
+  token: String,
+  hash: String,
+  salt: String,
+});
 module.exports = User;
